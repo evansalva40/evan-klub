183ebbcfe2d0d833b8d95bdfc994edd0611b12d5
evansalva40 <evansalva40@gmail.com>
2025-03-21 09:25:03 +0700

    Update neutron.py

--- neutron.py
+++ neutron.py
@@ -1,1 +1,1 @@
-Code is read more often than it is written.There’s no such thing as a simple software project.neutron.py
+Programming is just Googling the same thing over and over again.Code is read more often than it is written.There’s no such thing as a simple software project.neutron.py
